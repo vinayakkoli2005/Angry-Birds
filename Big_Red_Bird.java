9c2211eb3a04b24a3dca05f23352a180ada9f999
vinayakkoli2005 <147984816+vinayakkoli2005@users.noreply.github.com>
2024-10-27 21:16:11 +0530

    Delete Big_Red_Bird.java

--- Big_Red_Bird.java
+++
@@ -1,9 +0,0 @@
-package game.com;
-
-import com.badlogic.gdx.graphics.Texture;
-
-class Big_Red_Bird extends Bird {
-    public Big_Red_Bird(float x, float y , Texture texture, float height, float width , float health) {
-        super(health, texture, x, y, width, height);
-    }
-}
