c35555cc10a198233ab323b03f42078ef22276a6
vinayakkoli2005 <147984816+vinayakkoli2005@users.noreply.github.com>
2024-11-29 19:38:33 +0530

    Delete core directory

--- core/src/main/java/game/com/PIG_2.java
+++
@@ -1,9 +0,0 @@
-package game.com;
-
-import com.badlogic.gdx.graphics.Texture;
-
-class PIG_2 extends Pig {
-    public PIG_2(float x, float y ,Texture texture, float width, float height, float health) {
-        super(health, texture, x, y, width, height);
-    }
-}
--- core/src/main/java/game/com/PIG_3.java
+++
@@ -1,9 +0,0 @@
-package game.com;
-
-import com.badlogic.gdx.graphics.Texture;
-
-class PIG_3 extends Pig {
-    public PIG_3(float x, float y, Texture texture, float width, float height, float health) {
-        super(health, texture, x, y, width, height);
-    }
-}
